f58dcf865e0af979f787942eb34c3c96780999f1
MinexAutomation <minex.automation@gmail.com>
2019-12-16 10:47:06 -0800

    Implemented the dotnet Visual Studio solution file operator. Improved project plan.

--- source/R5T.Smyni.Norsica/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Smyni.Norsica
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Smyni.Norsica/Code/Services/Implementations/DotnetVisualStudioSolutionFileOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Norsica;
+
+
+namespace R5T.Smyni.Norsica
+{
+    public class DotnetVisualStudioSolutionFileOperator
+    {
+        private IDotnetOperator DotnetOperator { get; }
+
+
+        public DotnetVisualStudioSolutionFileOperator(IDotnetOperator dotnetOperator)
+        {
+            this.DotnetOperator = dotnetOperator;
+        }
+    }
+}
