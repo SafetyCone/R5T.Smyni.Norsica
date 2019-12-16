d04a6935f73e801079e43c4c49b671d6c8ffa922
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:48:35 -0800

    Implemented the IDotnetOperator-based Visual Studio solution file operator.

--- source/R5T.Smyni.Norsica/Code/Services/Implementations/DotnetVisualStudioSolutionFileOperator.cs
+++ source/R5T.Smyni.Norsica/Code/Services/Implementations/DotnetVisualStudioSolutionFileOperator.cs
@@ -8,16 +8,22 @@ namespace R5T.Smyni.Norsica
     public class DotnetVisualStudioSolutionFileOperator : IVisualStudioSolutionFileOperator
     {
         private IDotnetOperator DotnetOperator { get; }
+        private IVisualStudioSolutionFilePathProvider VisualStudioSolutionFilePathProvider { get; }
 
 
-        public DotnetVisualStudioSolutionFileOperator(IDotnetOperator dotnetOperator)
+        public DotnetVisualStudioSolutionFileOperator(IDotnetOperator dotnetOperator, IVisualStudioSolutionFilePathProvider visualStudioSolutionFilePathProvider)
         {
             this.DotnetOperator = dotnetOperator;
+            this.VisualStudioSolutionFilePathProvider = visualStudioSolutionFilePathProvider;
         }
 
-        public void CreateNewSolutionFile(string solutionDirectoryPath, string solutionName)
+        public string CreateSolutionFile(string solutionDirectoryPath, string solutionName)
         {
             this.DotnetOperator.CreateNewSolutionFile(solutionDirectoryPath, solutionName);
+
+            // Get the path of the created solution file path.
+            var solutionFilePath = this.VisualStudioSolutionFilePathProvider.GetVisualStudioSolutionFilePath(solutionDirectoryPath, solutionName);
+            return solutionFilePath;
         }
     }
 }
